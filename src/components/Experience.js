9458d9dad8ae2918032e5bf70eef29393e203661
Jason Long <jlong5795@gmail.com>
2020-07-23 20:14:39 -0500

    Follow up on feedback from Katie

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -5,7 +5,7 @@ const Experience = () => {
     <section className="experienceContainer">
       <h3 className="experienceTitle">A little about me</h3>
       <p className="experienceParagraph">
-        I am a full stack web developer born and raised in Chicago, Illinois. Most of my professional life has been spent in specialty retail and event management but I've always had an interest in technology and software development. I thoroughly enjoy identifying problems and opportunities for gains in efficiency then crafting solutions to those needs. I spend my free time gaming, working on person projects of various types and working with budding software developers. I also love meeting new people so I hope that we'll have a chance to become acquainted.
+        I am a full stack web developer born and raised in Chicago, Illinois. Most of my professional life has been spent in specialty retail and event management but I've always had an interest in technology and software development. I thoroughly enjoy identifying problems and opportunities for gains in efficiency then crafting solutions to those needs. In my free time you can find me gaming, working on person projects, and mentoring budding software developers. I also love meeting new people so I hope that we'll have a chance to become acquainted.
       </p>
     </section>
   );
